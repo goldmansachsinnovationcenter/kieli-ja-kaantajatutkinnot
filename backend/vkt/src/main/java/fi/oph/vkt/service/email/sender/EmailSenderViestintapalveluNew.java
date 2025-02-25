a0bc92cd6c2b982294e83b748c6065eedcc227cf
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2025-02-25 15:27:23 +0200

    VKT(Backend) Viestintäpalvelu code clean up

--- backend/vkt/src/main/java/fi/oph/vkt/service/email/sender/EmailSenderViestintapalveluNew.java
+++ backend/vkt/src/main/java/fi/oph/vkt/service/email/sender/EmailSenderViestintapalveluNew.java
@@ -3,6 +3,7 @@ package fi.oph.vkt.service.email.sender;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import fi.oph.vkt.config.Constants;
 import fi.oph.vkt.service.email.EmailAttachmentData;
 import fi.oph.vkt.service.email.EmailData;
 import fi.vm.sade.javautils.nio.cas.CasClient;
@@ -11,7 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.ExecutionException;
 import lombok.RequiredArgsConstructor;
 import org.asynchttpclient.Request;
 import org.asynchttpclient.RequestBuilder;
@@ -33,8 +33,7 @@ public class EmailSenderViestintapalveluNew implements EmailSender {
   private final String apiUrl;
 
   @Override
-  public String sendEmail(final EmailData emailData)
-    throws JsonProcessingException, ExecutionException, InterruptedException {
+  public String sendEmail(final EmailData emailData) throws JsonProcessingException {
     final ObjectMapper objectMapper = new ObjectMapper();
     final List<String> attachments = createAndPostAttachments(emailData.attachments());
     final Map<String, Object> postData = createPostData(emailData, attachments);
@@ -55,19 +54,18 @@ public class EmailSenderViestintapalveluNew implements EmailSender {
 
       if (response.getStatusCode() == HttpConstants.ResponseStatusCodes.OK_200) {
         return parseExternalId(response.getResponseBody(), "lahetysTunniste");
+      } else {
+        throw new RuntimeException(
+          String.format("Failed to post send email with return status %s", response.getStatusCode())
+        );
       }
-    } catch (Exception e) {
-      throw e;
+    } catch (final Exception e) {
+      throw new RuntimeException(e);
     }
-
-    return null;
   }
 
   private Map<String, Object> createPostData(final EmailData emailData, final List<String> attachments) {
-    final Map<String, Object> senderFields = Map.of("nimi", sender, "sahkopostiOsoite", "noreply@opintopolku.fi");
-    // Allowed characters: a-z, A-Z, 0-9 ja -_.
-    final String emailKeyPrefix = "vkt-email-";
-    final Integer expirationDays = 180; // 6 months
+    final Map<String, Object> senderFields = Map.of("nimi", sender, "sahkopostiOsoite", Constants.EMAIL_SENDER_ADDRESS);
 
     final List<Map<String, String>> recipientFields = List.of(
       Map.of("nimi", emailData.recipientName(), "sahkopostiOsoite", emailData.recipientAddress())
@@ -89,16 +87,15 @@ public class EmailSenderViestintapalveluNew implements EmailSender {
       "prioriteetti",
       "normaali",
       "sailytysaika",
-      expirationDays,
+      Constants.EMAIL_EXPIRATION_DAYS,
       "idempotencyKey",
-      emailKeyPrefix + emailData.id(),
+      Constants.EMAIL_ID_PREFIX + emailData.id(),
       "liitteidenTunnisteet",
       attachments
     );
   }
 
-  private String postAttachment(final EmailAttachmentData attachment)
-    throws ExecutionException, InterruptedException, JsonProcessingException {
+  private String postAttachment(final EmailAttachmentData attachment) {
     final Request request = new RequestBuilder()
       .setUrl(apiUrl + "/lahetys/v1/liitteet")
       .setMethod("POST")
@@ -116,15 +113,17 @@ public class EmailSenderViestintapalveluNew implements EmailSender {
         final String id = parseExternalId(response.getResponseBody(), "liiteTunniste");
 
         if (id == null) {
-          throw new RuntimeException("");
+          throw new RuntimeException(String.format("Failed to receive id for attachment %s", attachment.name()));
         }
 
         return id;
       } else {
-        throw new RuntimeException("");
+        throw new RuntimeException(
+          String.format("Failed to post email attachment with return status %s", response.getStatusCode())
+        );
       }
-    } catch (Exception e) {
-      throw e;
+    } catch (final Exception e) {
+      throw new RuntimeException(e);
     }
   }
 
